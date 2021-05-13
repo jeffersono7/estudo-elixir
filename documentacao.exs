e2c2d432dcd243152ff19d97fbf0007f7e96985a
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-13 06:45:16 -0300

    documentacao de modulos e funcoes

--- documentacao.exs
+++ documentacao.exs
@@ -2,3 +2,56 @@
 
 
 # - anotações 
+
+# - documentação em linha
+
+#----
+# Outputs 'Hello, chum.' to the console.
+IO.puts "Hello, " <> "chum."
+
+#----
+
+
+# - Documentação de Módulos
+
+# -----
+defmodule Greeter do
+  @moduledoc """
+  Provides a funcion `hello/1` to greet a human
+  """
+
+  def hello(name) do
+    "Hello, " <> name
+  end
+end
+# -----
+
+# Documentação de Funções
+# -----
+defmodule Greeter do
+  @moduledoc """
+  ...
+  """
+
+  @doc """
+  Prints a hello message
+
+  ## Parameters
+   - name: String that represents the name of the person.
+
+  ## Examples
+
+    iex> Greeter.hello("Sean")
+    "Hello, Sean"
+
+    iex> Greeter.hello("pete")
+    "Hello, pete"
+
+  """
+  @spec hello(String.t()) :: String.t()
+  def hello(name) do
+    "Hello, " <> name
+  end
+end
+# -----
+
