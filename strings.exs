f3e74953ea23cd992a3ead81deabeb4a355ab84e
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-15 08:52:17 -0300

    string in elixir

---
+++ strings.exs
@@ -0,0 +1,17 @@
+# Strings
+
+# Strings, listas de caracteres, Graphemes e Codepoints, Funções de Strings.
+
+defmodule Anagram do
+  def anagrams?(a, b) when is_binary(a) and is_binary(b) do
+    sort_string(a) == sort_string(b)
+  end
+
+  def sort_string(string) do
+    string
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.sort()
+  end
+end
+
