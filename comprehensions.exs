d7bf00fe41d1c7ff11ca1118483f47080c86a306
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-15 07:23:03 -0300

    comprehensions in elixir

---
+++ comprehensions.exs
@@ -0,0 +1,9 @@
+# Comprehensions
+
+
+# Comprehensions são um 'syntectic sugar' (uma forma mais simples de escrever) para realizar loops em Enumerables em Elixir. Nesta lição veremos como podemos fazer iterações e gerar os resultados utilizando utilizando comprehensions.
+
+# básico
+# filtros
+# usando :into
+
