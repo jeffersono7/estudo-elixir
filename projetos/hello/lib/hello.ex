fb779330d22daa6c2eba54f27b49edcf8023cfa8
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-19 06:28:24 -0300

    mix tasks

---
+++ projetos/hello/lib/hello.ex
@@ -0,0 +1,13 @@
+defmodule Hello do
+  @moduledoc """
+  Documentation for `Hello`.
+  """
+
+  @doc """
+  Outputs `Hello, World!` every time.
+  """
+  def say do
+    IO.puts "Hello, World!"
+  end
+end
+
